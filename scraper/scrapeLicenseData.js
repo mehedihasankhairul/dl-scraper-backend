3d99b350b3e51c9b708b189bd25de747f39870fe
Mehedi Hasan Khairul <mhkhairul78@gmail.com>
2024-11-02 23:23:50 +0600

    trying with postinstallation

--- scraper/scrapeLicenseData.js
+++ scraper/scrapeLicenseData.js
@@ -43,7 +43,7 @@ async function scrapeLicenseData(refNo) {
     browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      defaultViewport: { width: 1920, height: 1080 },
+      // defaultViewport: { width: 1920, height: 1080 },
     });
 
     const page = await browser.newPage();
